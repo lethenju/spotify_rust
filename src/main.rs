af2664b23482e3b533727bd4f25f8dc72b1db750
Julien LE THENO <julien.letheno@gmail.com>
2018-11-19 19:05:36 +0200

    Layouting interface, constructing playlist from user...

--- src/main.rs
+++ src/main.rs
@@ -1,40 +1,62 @@
 extern crate failure;
 extern crate termion;
 extern crate tui;
+
+#[allow(dead_code)]
 mod easy_api;
 mod interface;
 
-
 pub use self::easy_api::EasyAPI;
-use std::io;
-use interface::App;
+use interface::{App, Tracks};
+use std::io::{self, BufRead};
 use termion::event::Key;
+use termion::input::MouseTerminal;
 use termion::raw::IntoRawMode;
+use termion::screen::AlternateScreen;
 use tui::backend::TermionBackend;
-use tui::layout::Layout;
+use tui::layout::{Alignment, Constraint, Corner, Direction, Layout, Rect};
 use tui::style::{Color, Modifier, Style};
-use tui::widgets::{Block, Borders, SelectableList, Widget};
+use tui::widgets::{Block, Borders, List, Paragraph, SelectableList, Text, Widget};
 use tui::Terminal;
 
-use interface::util::event::{Event, Events};
-
+use interface::util::{Event, Events};
 
 fn main() -> Result<(), failure::Error> {
+    let mut search = String::new();
+    {
+        println!("Search for a playlist :");
+        let stdin = io::stdin();
+        let mut iterator = stdin.lock().lines();
+        search = iterator.next().unwrap().unwrap().to_string();
+    }
+
+    println!("Ok, lets find {}", search);
+
+    let mut easy_api = EasyAPI::construct();
+    easy_api.refresh();
+
+    let mut final_results = Vec::new();
+    easy_api.search("playlist", search.as_str(), &mut final_results);
+
+    // App
+    let mut items = Vec::<&str>::new();
+    for item in &final_results {
+        items.push(item);
+    }
+
     // Terminal initialization
     let stdout = io::stdout().into_raw_mode()?;
+    let stdout = MouseTerminal::from(stdout);
+    let stdout = AlternateScreen::from(stdout);
     let backend = TermionBackend::new(stdout);
     let mut terminal = Terminal::new(backend)?;
     terminal.hide_cursor()?;
 
     let events = Events::new();
-
     // App
-    let mut app = App::new();
+    let mut app = App::new(items);
+    let mut tracks  = Tracks::new();
 
-
-    let mut easy_api = EasyAPI::construct();
-    easy_api.refresh();
-                    
     loop {
         let size = terminal.size()?;
         if size != app.size {
@@ -43,19 +65,56 @@ fn main() -> Result<(), failure::Error> {
         }
 
         terminal.draw(|mut f| {
-            let chunks = Layout::default();
-                //.constraints([Constraint::Percentage(50), Constraint::Percentage(50)].as_ref())
-                //.split(app.size);
+            let chunks = Layout::default()
+                .direction(Direction::Vertical)
+                .constraints([Constraint::Percentage(80), Constraint::Percentage(20)].as_ref())
+                .split(app.size);
 
             let style = Style::default().fg(Color::Green).bg(Color::DarkGray);
+
+            let chunks_middle = Layout::default()
+                .direction(Direction::Horizontal)
+                .constraints([Constraint::Percentage(30), Constraint::Percentage(70)].as_ref())
+                .split(chunks[0]);
+
             SelectableList::default()
-                .block(Block::default().borders(Borders::ALL).title("Playlist chooser"))
-                .items(&app.items)
+                .block(
+                    Block::default()
+                        .borders(Borders::ALL)
+                        .title("Playlists"),
+                ).items(&app.items)
                 .select(app.selected)
                 .style(style)
                 .highlight_style(style.fg(Color::White).modifier(Modifier::Bold))
                 .highlight_symbol(">")
-                .render(&mut f, app.size);
+                .render(&mut f, chunks_middle[0]);
+
+
+            SelectableList::default()
+                .block(
+                    Block::default()
+                        .borders(Borders::ALL)
+                        .title("Tracks"),
+                ).items(&tracks.items)
+                .select(tracks.selected)
+                .style(style)
+                .highlight_style(style.fg(Color::White).modifier(Modifier::Bold))
+                .highlight_symbol(">")
+                .render(&mut f, chunks_middle[1]);
+
+            let text = [
+                Text::styled(
+                    "Artist : \n",
+                    Style::default().fg(Color::White).modifier(Modifier::Bold),
+                ),
+                Text::styled("Track :\n", Style::default().fg(Color::White)),
+            ];
+            Paragraph::new(text.iter())
+                .block(Block::default().title("Now Playing").borders(Borders::ALL))
+                .style(Style::default().fg(Color::White).bg(Color::Black))
+                .alignment(Alignment::Center)
+                .wrap(true)
+                .render(&mut f, chunks[1])
         })?;
 
         match events.next()? {
@@ -89,7 +148,6 @@ fn main() -> Result<(), failure::Error> {
                     }
                 }
                 Key::Right => {
-
                     app.selected = if let Some(selected) = app.selected {
                         easy_api.search_and_play_first("playlist", app.items[selected]);
                         Some(selected)
@@ -100,6 +158,7 @@ fn main() -> Result<(), failure::Error> {
                 _ => {}
             },
             Event::Tick => {
+                app.advance();
             }
         }
     }
