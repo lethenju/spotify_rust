f17ff3d1e2c5101fae4d76024312021db03ce4bf
Julien LE THENO <julien.letheno@gmail.com>
2018-11-22 17:44:00 +0200

    get track from album test

--- spotify_api/tests/tests.rs
+++ spotify_api/tests/tests.rs
@@ -12,7 +12,9 @@ fn search_tracks() {
     let mut handle = EasyAPI::new();
     handle.refresh();
     let mut billie_jean_results = Vec::new();
-    handle.search_track("billie jean", &mut billie_jean_results).unwrap();
+    handle
+        .search_track("billie jean", &mut billie_jean_results)
+        .unwrap();
     assert!(billie_jean_results.len() > 0);
     for track in billie_jean_results {
         println!("TRACKS {} : {}", track.id, track.name);
@@ -24,7 +26,9 @@ fn search_albums() {
     let mut handle = EasyAPI::new();
     handle.refresh();
     let mut thriller_results = Vec::new();
-    handle.search_album("thriller", &mut thriller_results).unwrap();
+    handle
+        .search_album("thriller", &mut thriller_results)
+        .unwrap();
     assert!(thriller_results.len() > 0);
     for album in thriller_results {
         println!("ALBUMS {} : {}", album.id, album.name);
@@ -36,7 +40,9 @@ fn search_artists() {
     let mut handle = EasyAPI::new();
     handle.refresh();
     let mut mj_results = Vec::new();
-    handle.search_artist("Michael Jackson", &mut mj_results).unwrap();
+    handle
+        .search_artist("Michael Jackson", &mut mj_results)
+        .unwrap();
     assert!(mj_results.len() > 0);
     for artist in mj_results {
         println!("ARTIST {} : {}", artist.id, artist.name);
@@ -48,7 +54,9 @@ fn search_playlists() {
     let mut handle = EasyAPI::new();
     handle.refresh();
     let mut mj_results = Vec::new();
-    handle.search_playlist("Michael Jackson", &mut mj_results).unwrap();
+    handle
+        .search_playlist("Michael Jackson", &mut mj_results)
+        .unwrap();
     assert!(mj_results.len() > 0);
     for playlist in mj_results {
         println!("PLAYLIST {} : {}", playlist.id, playlist.name);
@@ -60,6 +68,41 @@ fn play_without_context() {
     let mut handle = EasyAPI::new();
     handle.refresh();
     let mut billie_jean_results = Vec::new();
-    handle.search_track("billie jean", &mut billie_jean_results).unwrap();
+    handle
+        .search_track("billie jean", &mut billie_jean_results)
+        .unwrap();
     handle.play_track(&billie_jean_results[0], None).unwrap();
+}
+
+#[test]
+fn play_with_context() {
+    let mut handle = EasyAPI::new();
+    handle.refresh();
+    let mut thriller_results = Vec::new();
+    let mut track_results = Vec::new();
+    handle
+        .search_album("thriller", &mut thriller_results)
+        .unwrap();
+    handle
+        .get_tracks_from_album(&thriller_results[0].id, &mut track_results)
+        .unwrap();
+    handle
+        .play_track(&track_results[0], Some(&thriller_results[0]))
+        .unwrap();
+}
+
+#[test]
+fn get_tracks_from_album() {
+    let mut handle = EasyAPI::new();
+    handle.refresh();
+    let mut thriller_results = Vec::new();
+    let mut track_results = Vec::new();
+    handle
+        .search_album("thriller", &mut thriller_results)
+        .unwrap();
+    handle
+        .get_tracks_from_album(&thriller_results[0].id, &mut track_results)
+        .unwrap();
+    assert!(track_results.len() > 0);
+    assert!(track_results[3].name == "Thriller"); // the 4rth track of the Thriller album is Thriller
 }
