3dedccbb9cad8a1d7e04aca50c9e5d04c5918cda
Julien LE THENO <julien.letheno@gmail.com>
2018-12-07 09:33:51 +0100

    Token retrieval in a separate module

--- src/main.rs
+++ src/main.rs
@@ -7,12 +7,12 @@ extern crate text_io;
 
 #[allow(dead_code)]
 mod interface;
+mod token_retrieval;
 
 use interface::util::{Event, Events};
 use interface::{Albums, Tracks};
 pub use spotify_api::EasyAPI;
-use std::fs::{File, OpenOptions};
-use std::io::{self, Write};
+use std::io;
 use termion::event::Key;
 use termion::input::MouseTerminal;
 use termion::raw::IntoRawMode;
@@ -28,39 +28,8 @@ fn main() -> Result<(), failure::Error> {
     let mut easy_api = EasyAPI::new();
     match easy_api.refresh() {
         Ok(()) => {}
-        Err(err) => {
-            println!("Refresh failed : {:?}", err.kind());
-
-            // TODO reorganize better
-            println!("Automatic token retrieval procedure activated");
-            println!("Enter the clientid of the application");
-            let clientid: String = text_io::read!("{}\n");
-            // TODO Only ask secret, and rebuild base64 from that
-            println!("Enter the base64 of the clientid:clientsecret here. A browser will open and ask you to connect to your Spotify Account");
-            println!("You'll have to copy the token code in the URL and to paste it here");
-            let base64: String = text_io::read!("{}\n");
-            File::create("base_64_secret").unwrap();
-            let mut f = OpenOptions::new()
-                .append(true)
-                .write(true)
-                .open("base_64_secret")
-                .unwrap();
-            f.write(base64.as_bytes()).unwrap();
-
-            webbrowser::open(&format!("https://accounts.spotify.com/authorize/?client_id={}&response_type=code&redirect_uri=http%3A%2F%2Flocalhost%2F&scope=user-read-private%20user-read-email%20playlist-read-private%20playlist-read-collaborative%20playlist-modify-public%20playlist-modify-private%20user-follow-modify%20user-follow-read%20user-library-read%20user-library-modify%20user-read-private%20user-read-birthdate%20user-read-email%20user-top-read%20ugc-image-upload%20user-read-playback-state%20user-modify-playback-state%20user-read-currently-playing%20user-read-recently-played",clientid.as_str())).unwrap();
-            println!("Paste now the token : ");
-            let code: String = text_io::read!("{}\n");
-            // TODO doesnt work from now !! We dont really have a refresh token but only a code. We need to POST a request 
-            // to have the token bacK
-                // File::create("refresh_token").unwrap();
-                // let mut f = OpenOptions::new()
-                //     .append(true)
-                //     .write(true)
-                //     .open("refresh_token")
-                //     .unwrap();
-                // f.write(refresh_token.as_bytes()).unwrap();
-                
-                // easy_api.refresh().unwrap();
+        Err(_err) => {
+            token_retrieval::retrieve_tokens().unwrap();
         }
     }
 
