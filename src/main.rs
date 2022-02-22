a4743fc1d73df977607955c3a65fe0c825cc2487
Julien Le-Theno <julien.le-theno@engie.com>
2022-02-22 13:38:08 +0100

    Use a context struct for data

--- src/main.rs
+++ src/main.rs
@@ -11,33 +11,61 @@ use imgui::*;
 use std::thread;
 use std::sync::mpsc;
 use std::sync::{Arc, Mutex};
+use spotify_api::model::album::SimplifiedAlbum;
 use spotify_api::model::album::FullAlbum;
 use spotify_api::model::album::SimplifiedAlbumWithTracks;
 use spotify_api::model::artist::SimplifiedArtist;
+use spotify_api::model::track::SimplifiedTrack;
+use spotify_api::model::track::FullTrack;
 use std::ffi::OsStr;
+
+struct UiState{
+    filter_album : String,
+    artists_displayed : Vec<SimplifiedArtist>,
+    albums_displayed : Vec<SimplifiedAlbumWithTracks>,
+}
+struct PlayingContext{
+    current_artist : Mutex<Option<SimplifiedArtist>>,
+    current_track : Mutex<Option<SimplifiedTrack>>,
+    current_track_full : Mutex<Option<FullTrack>>,
+    current_album : Mutex<Option<SimplifiedAlbum>>,
+}
+struct AppContext{
+    ui_state : UiState,
+    playing_context : PlayingContext,
+    albums_data : Vec<FullAlbum>,
+    easy_api : Arc<Mutex<EasyAPI>>,
+}
+
 
 fn main() -> Result<(), failure::Error> {
     //let mut easy_api.lock().unwrap() =
-    let easy_api = Arc::new(Mutex::new(EasyAPI::new()));
-    match easy_api.lock().unwrap().refresh() {
+    let mut app = AppContext{
+        ui_state : UiState {
+            filter_album : String::new(),
+            artists_displayed : Vec::new(),
+            albums_displayed : Vec::new(),
+        },
+        playing_context : PlayingContext{
+            current_artist : Mutex::new(None::<SimplifiedArtist>),
+            current_track : Mutex::new(None::<SimplifiedTrack>),
+            current_track_full : Mutex::new(None::<FullTrack>),
+            current_album : Mutex::new(None::<SimplifiedAlbum>),
+        },
+        easy_api : Arc::new(Mutex::new(EasyAPI::new())),
+        albums_data : Vec::new(),
+    };
+
+    match app.easy_api.lock().unwrap().refresh() {
         Ok(()) => {}
         Err(_err) => {
-            token_retrieval::retrieve_tokens(&mut easy_api.lock().unwrap()).unwrap();
-            easy_api.lock().unwrap().refresh().unwrap();
+            token_retrieval::retrieve_tokens(&mut app.easy_api.lock().unwrap()).unwrap();
+            app.easy_api.lock().unwrap().refresh().unwrap();
         }
     }
 
-    // Total album library
-    let mut albums_data : Vec<FullAlbum> = Vec::new();
-
-    let mut filter_album = String::new();
-    // Albums actually displayed on the screen
-    let mut albums_displayed : Vec<SimplifiedAlbumWithTracks> = Vec::new();
-    // Artists actually displayed on the screen
-    let mut artists_displayed : Vec<SimplifiedArtist> = Vec::new();
-
     let (tx, rx) = mpsc::channel();
-    let (easy_api_thread, tx_thread) = (Arc::clone(&easy_api),tx.clone());
+    let (easy_api_thread, tx_thread) = (Arc::clone(&app.easy_api),tx.clone());
     let _handle = thread::spawn(move || {
         let mut ended = false;
         let mut i = 0;
@@ -55,11 +83,11 @@ fn main() -> Result<(), failure::Error> {
     });
 
     let mut system = support::init(file!());
-    let mut current_artist = Mutex::new(easy_api.lock().unwrap().get_currently_playing_artist().unwrap());
-    let mut current_track = Mutex::new(easy_api.lock().unwrap().get_currently_playing_track().unwrap());
-    let current_track_full = Mutex::new(easy_api.lock().unwrap().get_currently_playing_track_full().unwrap());
-    let current_track_full_guard = current_track_full.lock().unwrap();
-    let mut current_album = match &*current_track_full_guard {
+    app.playing_context.current_artist = Mutex::new(app.easy_api.lock().unwrap().get_currently_playing_artist().unwrap());
+    app.playing_context.current_track = Mutex::new(app.easy_api.lock().unwrap().get_currently_playing_track().unwrap());
+    app.playing_context.current_track_full = Mutex::new(app.easy_api.lock().unwrap().get_currently_playing_track_full().unwrap());
+    let current_track_full_guard = app.playing_context.current_track_full.lock().unwrap().clone();
+    app.playing_context.current_album = match current_track_full_guard {
         Some(track) => {Mutex::new(Some(track.album.clone()))},
         None => {Mutex::new(None)}
     };
@@ -80,7 +108,7 @@ fn main() -> Result<(), failure::Error> {
             let _roboto = ui.push_font(roboto);
             ui.text("Now listening to:");
             ui.spacing();
-            let current_artist_guard = current_artist.lock().unwrap();
+            let current_artist_guard = app.playing_context.current_artist.lock().unwrap();
             match &*current_artist_guard {
                 Some(artist) => {
                     if ui.button(&artist.name){
@@ -90,18 +118,18 @@ fn main() -> Result<(), failure::Error> {
                 }
                 None => {}
             };
-            let current_track_guard = current_track.lock().unwrap();
+            let current_track_guard = app.playing_context.current_track.lock().unwrap();
             match &*current_track_guard {
                 Some(track) => {
                     ui.text(&track.name);
-                    let current_album_local = current_album.lock().unwrap();
+                    let current_album_local = app.playing_context.current_album.lock().unwrap();
                     match &*current_album_local{
                         Some(album) => {
                             if ui.button(&album.name) {
                                 println!("Need to load album {} ", album.name);
-                                let mut new_album = SimplifiedAlbumWithTracks{data : album.clone(),tracks : Vec::new()};
+                                let new_album = SimplifiedAlbumWithTracks{data : album.clone(),tracks : Vec::new()};
                                 let mut is_present = false;
-                                for alb in &albums_displayed  {
+                                for alb in &app.ui_state.albums_displayed  {
                                     if alb.data.id == album.id {
                                         println!("Album {} already shown up", album.name);
                                         is_present = true;
@@ -109,7 +137,7 @@ fn main() -> Result<(), failure::Error> {
                                     }
                                 }
                                 if !is_present {
-                                    albums_displayed.push(new_album);
+                                    app.ui_state.albums_displayed.push(new_album);
                                 }
                             }
                         }
@@ -124,24 +152,38 @@ fn main() -> Result<(), failure::Error> {
         });
         Window::new("Spotify Albums")
         .size([500.0, 800.0], Condition::Appearing)
+        .menu_bar(true)
         .build(ui, || {
-            let _roboto = ui.push_font(roboto);
             if let Some(menu_bar) = ui.begin_menu_bar() {
-                if let Some(menu) = ui.begin_menu("Menu") {
-                    MenuItem::new("Filter").build(ui);
-                    if (ui.button("Filtrer par date de parution")){
+
+                if let Some(menu) = ui.begin_menu("Filters") {
+                    /*if (ui.button("Filtrer par date de parution")){
                         // C'est parti
+                    }*/
+
+                    menu.end();
+                }
+
+                if let Some(menu) = ui.begin_menu("Sort by") {
+
+                    if ui.button("Release Date"){
+                        // C'est parti
+                        app.albums_data.sort_by(|a, b| b.release_date.cmp(&a.release_date));
                     }
 
                     menu.end();
                 }
+
+                menu_bar.end();
             }
-            ui.input_text("", &mut filter_album).build();
-            for album in &albums_data {
+
+            ui.spacing();
+            ui.input_text("", &mut app.ui_state.filter_album).build();
+            for album in &app.albums_data {
                 let mut show = true;
-                if !filter_album.is_empty(){
+                if !app.ui_state.filter_album.is_empty(){
                     show = false;
-                    let my_os_filter = OsStr::new(&filter_album);
+                    let my_os_filter = OsStr::new(&app.ui_state.filter_album);
                     if album.name.to_string().contains(my_os_filter.to_str().unwrap()) ||
                        album.artists[0].name.to_string().contains(my_os_filter.to_str().unwrap()) {
                         show = true;
@@ -151,47 +193,45 @@ fn main() -> Result<(), failure::Error> {
                 if show {
                     if ui.button(format!("{} - {}", album.artists[0].name, album.name)){
                         println!("Need to load album {} ", album.name);
-                        let mut new_album = SimplifiedAlbumWithTracks{data : album.clone().to_simplified(),tracks : Vec::new()};
+                        let new_album = SimplifiedAlbumWithTracks{data : album.clone().to_simplified(),tracks : Vec::new()};
                         let mut is_present = false;
-                        for alb in &albums_displayed  {
+                        for alb in &app.ui_state.albums_displayed  {
                             if alb.data.id == album.id {
                                 is_present = true;
                                 break;
                             }
                         }
                         if !is_present {
-                            albums_displayed.push(new_album);
+                            app.ui_state.albums_displayed.push(new_album);
                         }
                     }
                 }
             }
 
             match rx.try_recv() {
-            Ok(albums) => { albums_data.extend(albums.clone())}
+            Ok(albums) => { app.albums_data.extend(albums.clone())}
             Err(_err) => {()}
             }
-
-            _roboto.pop();
         });
         let mut key_remove = 0;
-        for key in 0..albums_displayed.len() {
-            
-            Window::new(format!("{} - {}", albums_displayed[key].data.artists[0].name, albums_displayed[key].data.name))
+        for key in 0..app.ui_state.albums_displayed.len() {
+
+            Window::new(format!("{} - {}", app.ui_state.albums_displayed[key].data.artists[0].name, app.ui_state.albums_displayed[key].data.name))
             .size([500.0, 500.0], Condition::FirstUseEver)
             .build(ui, || {
 
-                let _roboto = ui.push_font(roboto);
-                if albums_displayed[key].tracks.len() == 0
+
+                if app.ui_state.albums_displayed[key].tracks.len() == 0
                 {
-                    let track_results = easy_api.lock().unwrap().get_tracks_from_album(&albums_displayed[key].data.id).unwrap();
-                    albums_displayed[key].tracks = track_results.clone();
+                    let track_results = app.easy_api.lock().unwrap().get_tracks_from_album(&app.ui_state.albums_displayed[key].data.id).unwrap();
+                    app.ui_state.albums_displayed[key].tracks = track_results.clone();
                 }
-                for track in &albums_displayed[key].tracks {
+                for track in &app.ui_state.albums_displayed[key].tracks {
                     if ui.button(format!("- {}",track.name)) {
-                        easy_api.lock().unwrap().play_track(track, Some(&albums_displayed[key].data)).unwrap();
-                        current_artist = Mutex::new(Some(albums_displayed[key].data.artists[0].clone()));
-                        current_track = Mutex::new(Some(track.clone()));
-                        current_album = Mutex::new(Some(albums_displayed[key].data.clone()));
+                        app.easy_api.lock().unwrap().play_track(track, Some(&app.ui_state.albums_displayed[key].data)).unwrap();
+                        app.playing_context.current_artist = Mutex::new(Some(app.ui_state.albums_displayed[key].data.artists[0].clone()));
+                        app.playing_context.current_track = Mutex::new(Some(track.clone()));
+                        app.playing_context.current_album = Mutex::new(Some(app.ui_state.albums_displayed[key].data.clone()));
 
                     }
                 }
@@ -201,12 +241,11 @@ fn main() -> Result<(), failure::Error> {
                     key_remove = key+1;
                 }
 
-                _roboto.pop();
             });
         }
         if key_remove > 0
         {
-            albums_displayed.remove(key_remove-1);
+            app.ui_state.albums_displayed.remove(key_remove-1);
         }
 
     });
